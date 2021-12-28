c80191a2c262b7464bff69c670efed42ab6c87a9
KD <do>
2021-12-28 16:26:35 +0100

    #94 - permissions renamed

--- src/Http/Requests/RoleUpdateRequest.php
+++ src/Http/Requests/RoleUpdateRequest.php
@@ -2,28 +2,26 @@
 
 namespace EscolaLms\Permissions\Http\Requests;
 
+use EscolaLms\Permissions\Enums\PermissionsPermissionsEnum;
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
 
 class RoleUpdateRequest extends FormRequest
 {
-
-
     /**
      * @return bool
      */
-    public function authorize()
+    public function authorize(): bool
     {
-        return Gate::check('administrate roles');
+        return Gate::check(PermissionsPermissionsEnum::PERMISSIONS_ROLE_MANAGE);
     }
-
 
     /**
      * Get the validation rules that apply to the request.
      *
      * @return array
      */
-    public function rules()
+    public function rules(): array
     {
         return [
             'permissions' => ['present', 'array'],
