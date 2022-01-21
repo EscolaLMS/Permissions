c04ecd05191586544cd8f798a797e5c6425c5216
KrzysztofDziedziechEscolasoft <96292232+KrzysztofDziedziechEscolasoft@users.noreply.github.com>
2022-01-21 15:42:10 +0100

    Feature/94 (#10)
    * #94 - permissions renamed
    
    * #94 - permissions permissions_role_list, permissions_role_read, permissions_role_create, permissions_role_update, permissions_role_delete
    
    * #94 - permissions permissions_role_list, permissions_role_read, permissions_role_create, permissions_role_update, permissions_role_delete
    
    * #94 - permission
    
    Co-authored-by: KD <do>
    Co-authored-by: krz <krzysztof.dziedziech@escola.pl>

--- src/Enums/PermissionsPermissionsEnum.php
+++ src/Enums/PermissionsPermissionsEnum.php
@@ -6,10 +6,10 @@ use EscolaLms\Core\Enums\BasicEnum;
 
 class PermissionsPermissionsEnum extends BasicEnum
 {
-    const PERMISSIONS_ROLE_MANAGE = 'permissions_role_manage';
-    const PERMISSIONS_ROLE_LIST = 'permissions_role_list';
-    const PERMISSIONS_ROLE_READ = 'permissions_role_read';
-    const PERMISSIONS_ROLE_CREATE = 'permissions_role_create';
-    const PERMISSIONS_ROLE_UPDATE = 'permissions_role_update';
-    const PERMISSIONS_ROLE_DELETE = 'permissions_role_delete';
+    const PERMISSIONS_ROLE_MANAGE = 'permission_role_manage';
+    const PERMISSIONS_ROLE_LIST = 'permission_role_list';
+    const PERMISSIONS_ROLE_READ = 'permission_role_read';
+    const PERMISSIONS_ROLE_CREATE = 'permission_role_create';
+    const PERMISSIONS_ROLE_UPDATE = 'permission_role_update';
+    const PERMISSIONS_ROLE_DELETE = 'permission_role_delete';
 }
