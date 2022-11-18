dc11b9d873f257ad7d9492cea9581297802b246f
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2022-11-18 16:27:58 +0100

    Add translations

---
+++ resources/lang/en/permissions.php
@@ -0,0 +1,12 @@
+<?php
+
+use EscolaLms\Permissions\Enums\PermissionsPermissionsEnum;
+
+return [
+    PermissionsPermissionsEnum::PERMISSIONS_ROLE_MANAGE => 'Manage role',
+    PermissionsPermissionsEnum::PERMISSIONS_ROLE_LIST => 'Role list',
+    PermissionsPermissionsEnum::PERMISSIONS_ROLE_READ => 'Read role',
+    PermissionsPermissionsEnum::PERMISSIONS_ROLE_CREATE => 'Create role',
+    PermissionsPermissionsEnum::PERMISSIONS_ROLE_UPDATE => 'Update role',
+    PermissionsPermissionsEnum::PERMISSIONS_ROLE_DELETE => 'Delete role',
+];
